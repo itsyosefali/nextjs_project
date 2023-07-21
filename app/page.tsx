fb0c078e44518fd072455cd2e5b2aadff6ce3666
itsyosefali <joeyxjoey123@gmail.com>
2023-07-21 15:19:04 +0200

    use data fetching in server components

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1>Hello World</h1>
      <Link href="/about">About Page</Link>
+     <Link href="/users">Users Page</Link>
     </main>
   )
 }
